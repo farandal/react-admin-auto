f245f53f546be72feb3035985a7725f4bf6e93a0
Felipe Baytelman <felipe.baytelman@gmail.com>
2018-10-18 19:54:42 -0400

    Default export

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-exports.ReactAdminAuto = require("./lib/AutoAdmin").AutoAdmin;
+exports.default = require("./lib/AutoAdmin").default;
+// exports.AutoAdmin = require("./lib/AutoAdmin").AutoAdmin;
+exports.AutoList = require("./lib/AutoAdmin").AutoList;
+exports.AutoShow = require("./lib/AutoAdmin").AutoShow;
+exports.AutoEdit = require("./lib/AutoAdmin").AutoEdit;
+exports.AutoCreate = require("./lib/AutoAdmin").AutoCreate;
