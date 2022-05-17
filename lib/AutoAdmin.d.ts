1a31849747b4c11e990c04ccc7bfa7b58afc3d70
Francisco Aranda <francisco.aranda@gsudo.cl>
2022-05-16 20:01:06 -0400

    added build

--- lib/AutoAdmin.d.ts
+++ lib/AutoAdmin.d.ts
@@ -1,27 +1,72 @@
+import { ReactNode } from 'react';
 import * as React from 'react';
-declare type ActionCallback = (id: string) => void;
-interface AutoAdminAttribute {
+export interface ICustomResourceSubComponent {
+    model?: string;
+    schema?: AutoAdminAttribute[];
+    references?: AutoAdminReference[];
+    label?: string;
+}
+export interface IAutoAdminCustomFieldComponent {
+    method: string;
+    attribute: AutoAdminAttribute;
+}
+export interface AutoAdminAttribute {
     attribute: string;
-    type: string | string[] | Object | DateConstructor | NumberConstructor | StringConstructor | AutoAdminAttribute[];
+    modelAttribute?: string;
+    type: React.FC<IRecord> | string | string[] | Object | DateConstructor | NumberConstructor | StringConstructor | AutoAdminAttribute[];
     tab?: string;
     label?: string;
     inList?: boolean;
     extended?: boolean;
     readOnly?: boolean;
+    isPassword?: boolean;
     fieldOptions?: any;
-    action?: ActionCallback | React.ComponentClass<IRecord>;
-    validate?: (value: any) => JSX.Element | string | undefined;
+    action?: any;
+    actionProps?: any;
+    validate?: (value: any, allValues: any) => JSX.Element | string | undefined;
+    pagination?: boolean;
+    multiple?: boolean;
+    component?: any;
+    searchField?: string;
+    listAttribute?: string;
+    custom?: boolean;
 }
-interface AutoAdminReference {
+export interface AutoAdminReference {
     reference: string;
     target: string;
     tab?: string;
     schema: AutoAdminAttribute[];
 }
-interface IRecord {
-    id: string;
+export interface IRecord {
+    id?: string;
     record?: any;
+    attribute?: AutoAdminAttribute;
+    [key: string]: any;
 }
+export declare const AttributeToField: (input: AutoAdminAttribute) => JSX.Element;
+export declare const AttributeToInput: (input: any) => JSX.Element;
+export declare const AutoFormTabs: (schema: AutoAdminAttribute[]) => JSX.Element[];
+export interface IAutoTabbedForm {
+    schema: AutoAdminAttribute[];
+    toolbar?: JSX.Element;
+    onSubmit?: (values: any) => any;
+    onError?: (error: any) => any;
+    mode?: "create" | "update";
+    children?: JSX.Element;
+}
+export declare const AutoTabbedForm: React.FC<IAutoTabbedForm>;
+export declare const AutoReferenceFormTab: (reference: AutoAdminReference) => JSX.Element;
+export declare const AutoReferenceTab: (reference: AutoAdminReference) => JSX.Element;
+export declare const AutoTabs: (schema: AutoAdminAttribute[]) => JSX.Element[];
+export interface ITabbedLayout {
+    schema: AutoAdminAttribute[];
+    references?: AutoAdminReference[];
+}
+export declare const validate: (schema: AutoAdminAttribute[]) => (values: {
+    [field: string]: string | JSX.Element;
+}) => {
+    [field: string]: string | JSX.Element;
+};
 export interface IReferenceFilter {
     label: string;
     source: string;
@@ -29,28 +74,63 @@ export interface IReferenceFilter {
     optionText: string;
 }
 export declare const AutoFilterGenerator: (referenceFilters?: IReferenceFilter[]) => (props: any) => JSX.Element;
-export declare const AutoCreate: (props: any, { schema }: {
+export interface IAutoTitle {
+    record?: any;
     schema: AutoAdminAttribute[];
-}) => JSX.Element;
-export declare const AutoShow: (props: any, { schema, references }: {
+}
+export declare const AutoTitle: React.FC<IAutoTitle>;
+export interface IAutoForm {
+    schema: AutoAdminAttribute[];
+    toolbar?: JSX.Element;
+    onSubmit?: (values: any) => any;
+    onError?: (error: any) => any;
+    mode: "create" | "update";
+    children?: any;
+}
+export declare const AutoCreate: React.FC<IAutoForm>;
+export interface IAutoShow {
     schema: AutoAdminAttribute[];
     references?: AutoAdminReference[];
-}) => JSX.Element;
-export declare const AutoEdit: (props: any, { schema }: {
+}
+export declare const AutoShow: React.FC<IAutoShow>;
+export declare const AutoEdit: React.FC<IAutoForm>;
+export interface IAutoDataGrid {
+    record?: any;
     schema: AutoAdminAttribute[];
-}) => JSX.Element;
-export declare const AutoDataGrid: (props: any, { schema }: {
-    schema: AutoAdminAttribute[];
-}) => JSX.Element;
-export declare const AutoList: (props: any, { schema, exporter, referenceFilters }: {
+    resource?: string;
+}
+export declare const AutoDataGrid: React.FC<IAutoDataGrid>;
+export interface IAutoList {
     schema: AutoAdminAttribute[];
     exporter?: any;
     referenceFilters?: IReferenceFilter[];
-}) => JSX.Element;
-export declare const AutoResource: (modelName: string, { schema, references, exporter, referenceFilters }: {
+    record?: any;
+    resource?: string;
+}
+export declare const AutoList: React.FC<IAutoList>;
+export interface IAutoResource {
+    model: string;
+    label: string;
+    show?: boolean;
+    list?: boolean;
+    edit?: boolean;
+    create?: boolean;
     schema: AutoAdminAttribute[];
     references?: AutoAdminReference[];
     exporter?: any;
     referenceFilters?: IReferenceFilter[];
+}
+export declare const AutoResource: ({ model, label, schema, references, exporter, referenceFilters, show, list, edit, create, icon, group }: {
+    model: string;
+    label: string;
+    schema: AutoAdminAttribute[];
+    references?: AutoAdminReference[];
+    exporter?: any;
+    referenceFilters?: IReferenceFilter[];
+    show?: boolean | React.FC;
+    list?: boolean | React.FC;
+    edit?: boolean | React.FC;
+    create?: boolean | React.FC;
+    icon?: any | JSX.Element | React.ReactNode;
+    group?: string;
 }) => JSX.Element;
-export {};
